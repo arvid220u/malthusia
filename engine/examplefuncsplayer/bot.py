692f76bfd422b2fd6f70e2a4ad22f1e9b2cd1472
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2021-09-18 00:02:18 -0400

    reformat

--- engine/examplefuncsplayer/bot.py
+++ engine/examplefuncsplayer/bot.py
@@ -1,5 +1,5 @@
-
 from malthusia.stubs import *
+
 
 # This is an example bot written by the developers!
 # Use this to help write your own code, or run it against your bot to see how well you can do!
